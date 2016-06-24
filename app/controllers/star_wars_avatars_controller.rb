227f084876701a46cc9ffc4d68df93af8ed17fa7
Paul Campbell <paul@rslw.com>
2016-06-24 08:54:19 +0100

    add health

--- app/controllers/star_wars_avatars_controller.rb
+++ app/controllers/star_wars_avatars_controller.rb
@@ -1,16 +1,20 @@
 class StarWarsAvatarsController < ApplicationController
 
-    def show
-      @starwavatar_avatar = StarwavatarAvatar.new(params[:id])
-      respond_to do |wants|
-        wants.svg  { render inline: @starwavatar_avatar.svg }
-        wants.png  do
-          send_data @starwavatar_avatar.png, filename: "#{params[:id]}.png",
-            disposition: 'inline',
-            quality: 90,
-            type: "image/png"
-        end
+  def health
+    head :ok
+  end
+
+  def show
+    @starwavatar_avatar = StarwavatarAvatar.new(params[:id])
+    respond_to do |wants|
+      wants.svg  { render inline: @starwavatar_avatar.svg }
+      wants.png  do
+        send_data @starwavatar_avatar.png, filename: "#{params[:id]}.png",
+          disposition: 'inline',
+          quality: 90,
+          type: "image/png"
       end
     end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get '/health', to: 'star_wars_avatars#health'
+
   get '/avatars/:id', to: 'star_wars_avatars#show', as: :avatar, defaults: {format: :svg}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
